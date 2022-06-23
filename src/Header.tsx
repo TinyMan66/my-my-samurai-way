ed9e4ba8abed896da8c16ba9921920562c3c5c53
Diana Lipanova <altere2804@gmail.com>
2022-06-23 22:01:38 +0300

    Initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return <div>
+        <a href='#s'>Home</a>
+        <a href='#s'>New Feed</a>
+        <a href='#s'>Massages</a>
+    </div>
+}
+
+export default Header;
