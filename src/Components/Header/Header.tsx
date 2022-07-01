4888c19ba37ec3bc1c9d7d26473289aad974771c
Diana Lipanova <altere2804@gmail.com>
2022-07-01 14:06:19 +0300

    Changed picture and size

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,6 @@ import s from './Header.module.css'
 
 export const Header = () => {
     return <header className={s.header}>
-        <img src='https://img1.fonwall.ru/o/nh/sun-solar-circle-corona.jpeg'/>
+        <img src='https://papik.pro/uploads/posts/2021-09/1631506537_3-papik-pro-p-konturnii-risunok-gori-5.jpg'/>
     </header>
 }
