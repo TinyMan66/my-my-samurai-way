15fa2c5e52301bda60ede8d0005acf6b95c54636
Diana Lipanova <altere2804@gmail.com>
2022-07-13 16:27:05 +0300

    Added text-area to message component

--- src/Components/Dialogs/Message/Message.tsx
+++ src/Components/Dialogs/Message/Message.tsx
@@ -5,8 +5,21 @@ type MessageType = {
     message: string
 }
 
+let newMessage = React.createRef<HTMLTextAreaElement>();
+let addMassageHandler = () => {
+    let textMassage = newMessage.current?.value;
+    alert(textMassage);
+}
+
 export const Message = (props: MessageType) => {
     return (
-        <div className={s.message}>{props.message}</div>
+        <div>
+            <div className={s.message}>{props.message}</div>
+            <div>
+                <textarea ref={newMessage}></textarea>
+                <button onClick={addMassageHandler}>Add Message</button>
+            </div>
+        </div>
+
     )
 }
