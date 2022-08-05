acc19153c8a70283c87b68dff386738b67c8b28c
Diana Lipanova <altere2804@gmail.com>
2022-08-05 17:39:51 +0300

    Connected redux

---
+++ src/redux/redux-store.ts
@@ -0,0 +1,17 @@
+import {combineReducers, legacy_createStore as createStore} from "redux";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
+const reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer
+});
+
+export let store = createStore(reducers);
+
+
+
+
+
