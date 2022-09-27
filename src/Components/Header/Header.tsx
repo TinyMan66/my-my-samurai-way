293d66ce267fffd698d2ab730730ecf56c8a2cfc
Diana Lipanova <altere2804@gmail.com>
2022-09-27 13:07:25 +0200

    Added log in to header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import s from './Header.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Header = () => {
     return <header className={s.header}>
-        <img src='https://papik.pro/uploads/posts/2021-09/1631506537_3-papik-pro-p-konturnii-risunok-gori-5.jpg'/>
+        <img src='https://papik.pro/uploads/posts/2021-09/1631506537_3-papik-pro-p-konturnii-risunok-gori-5.jpg' alt={'wallpaper'}/>
+
+        <div className={s.loginBlock}>
+            <NavLink to={'/login'}>Log in</NavLink>
+        </div>
     </header>
 }
