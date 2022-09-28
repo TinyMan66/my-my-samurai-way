54fb220e0b356b9cd175babce2cbf9b1914fc145
Diana Lipanova <altere2804@gmail.com>
2022-09-28 16:36:37 +0200

    Refactored auth-reducer.tsx

--- src/redux/auth-reducer.tsx
+++ src/redux/auth-reducer.tsx
@@ -1,33 +1,41 @@
 import React from 'react'
 
-export type UsersActionCreatorTypes =
-    ReturnType<typeof setUserData>
+export type AuthActionCreatorTypes = ReturnType<typeof setAuthUserData>
 
-
-export const setUserData = (userId: string, email: string, login: string) => (
+export const setAuthUserData = (userId: number, email: string, login: string) => (
     {type: 'SET-USER-DATA', data: {userId, email, login}} as const);
 
 
-export type AuthType = {
-    userId: string
+export type DataAuthType = {
+    userId: number
     email: string
     login: string
 }
 
+// export type AuthType = {
+//     data: Array<DataAuthType>,
+//     resultCode: number | null,
+//     messages: Array<string>
+// }
+
 export type initialStateType = typeof initialState;
 
 let initialState = {
-    userId: " ",
-    email: " ",
-    login: " "
+    data: {
+        userId: NaN,
+        email: "",
+        login: ""
+    },
+    isAuth: false
 }
 
-const authReducer = (state: initialStateType = initialState, action: UsersActionCreatorTypes): initialStateType => {
+const authReducer = (state: initialStateType = initialState, action: AuthActionCreatorTypes): initialStateType => {
     switch (action.type) {
         case 'SET-USER-DATA':
             return {
                 ...state,
-                ...action.data
+                data: action.data,
+                isAuth: true
             }
         default:
             return state;
