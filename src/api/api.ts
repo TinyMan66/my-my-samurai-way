f9e339ad4ff14edf25bc2c74e600b8e141bfca49
Diana Lipanova <altere2804@gmail.com>
2022-09-30 17:16:14 +0200

    Refactored api methods

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,27 +1,23 @@
 import axios from "axios";
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "ccd72c64-da46-4e68-ace8-41333a566ebf"
+    }
+});
 
 export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-        withCredentials: true
-    })
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data);
 }
 
 export const unfollow = (id: number = 1) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`,{
-        withCredentials: true,
-        headers: {
-            "API-KEY": "ccd72c64-da46-4e68-ace8-41333a566ebf"
-        }
-    })
+    return instance.delete(`follow/${id}`)
 }
+
 export const follow = (id: number = 1) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "ccd72c64-da46-4e68-ace8-41333a566ebf"
-        }
-    })
+    return instance.post(`follow/${id}`)
 }
 
