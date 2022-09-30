3429593a6d83a56b62ecde1a446441365f9fcde8
Diana Lipanova <altere2804@gmail.com>
2022-09-30 16:52:31 +0200

    Selected part of the data from the response for the component.

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -39,22 +39,19 @@ class UsersContainer extends React.Component<UsersPropsType> {
 
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        getUsers(this.props.currentPage, this.props.pageSize).then(response => {
+        getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(response.data.items)
-                this.props.setUsersTotalCount(response.data.totalCount);
+                this.props.setUsers(data.items)
+                this.props.setUsersTotalCount(data.totalCount);
             });
     }
 
     onPageChange = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-            withCredentials: true
-        })
-            .then(response => {
+        getUsers(pageNumber, this.props.pageSize).then(data => {
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(response.data.items);
+                this.props.setUsers(data.items);
             });
     }
 
--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,{
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
         withCredentials: true
     })
+        .then(response => response.data);
 }
