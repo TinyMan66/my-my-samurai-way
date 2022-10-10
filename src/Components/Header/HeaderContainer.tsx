f7459a4b727274817b19d71c82c08a9ccc782814
Diana Lipanova <altere2804@gmail.com>
2022-10-10 12:42:20 +0200

    Created authUser thunk to get auth data

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import {Header} from "./Header";
-import {setAuthUserData} from "../../redux/auth-reducer";
+import {authUser} from "../../redux/auth-reducer";
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
-import {usersAPI} from "../../api/api";
 
 type mapStatePropsType = {
     isAuth: boolean
@@ -12,7 +11,7 @@ type mapStatePropsType = {
 }
 
 type mapDispatchPropsType = {
-    setAuthUserData: (userId: number, email: string, login: string) => void
+    authUser: () => void
 }
 
 export type AuthPropsType = mapStatePropsType & mapDispatchPropsType;
@@ -20,27 +19,22 @@ export type AuthPropsType = mapStatePropsType & mapDispatchPropsType;
 class HeaderContainer extends React.Component<AuthPropsType> {
 
     componentDidMount() {
-       usersAPI.authUser().then(data => {
-           if (data.resultCode === 0) {
-               let {id, email, login} = data.data;
-               this.props.setAuthUserData(id, email, login)
-           }
-       })
+        this.props.authUser()
     }
 
     render() {
         return (
             <Header isAuth={this.props.isAuth}
-                       login={this.props.login}
-        />)
+                    login={this.props.login}
+            />)
     }
 }
 
-const mapStateToProps =(state: AppStateType): mapStatePropsType => {
+const mapStateToProps = (state: AppStateType): mapStatePropsType => {
     return {
         isAuth: state.auth.isAuth,
         login: state.auth.data.login
     }
 }
 
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer)
+export default connect(mapStateToProps, {authUser})(HeaderContainer)
--- src/redux/auth-reducer.tsx
+++ src/redux/auth-reducer.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Dispatch} from "redux";
+import {usersAPI} from "../api/api";
 
 export type AuthActionCreatorTypes = ReturnType<typeof setAuthUserData>
 
 export const setAuthUserData = (userId: number, email: string, login: string) => (
     {type: 'SET-USER-DATA', data: {userId, email, login}} as const);
 
+export const authUser = () => {
+    return (dispatch: Dispatch<AuthActionCreatorTypes>) => {
+        usersAPI.authUser().then(data => {
+            if (data.resultCode === 0) {
+                let {id, email, login} = data.data;
+                dispatch(setAuthUserData(id, email, login))
+            }
+        })
+    }
+}
 
 export type DataAuthType = {
     userId: number
