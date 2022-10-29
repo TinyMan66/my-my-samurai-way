e0f89b43b01b09a3dd9178c2eccf588223b0bf29
Diana Lipanova <altere2804@gmail.com>
2022-10-29 18:56:47 +0200

    Added error functionality

--- src/Components/common/FormsControls/FormsControls.tsx
+++ src/Components/common/FormsControls/FormsControls.tsx
@@ -3,12 +3,14 @@ import styles from "./FormsControls.module.css"
 import {WrappedFieldProps} from "redux-form";
 
 export const Textarea: FC<WrappedFieldProps> = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
+
     return (
-        <div className={styles.formControl}>
+        <div className={`${styles.formControl} ${hasError? styles.error: ""}` }>
             <div>
                 <textarea {...input} {...props}/>
             </div>
-            <span>{"Some error"}</span>
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
