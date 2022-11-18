f73a84dd44e4cef04ab96c50dd6ecf7a113c5fee
Diana Lipanova <altere2804@gmail.com>
2022-11-18 23:07:24 +0100

    Page change fixed

--- src/redux/users-reducer.tsx
+++ src/redux/users-reducer.tsx
@@ -32,10 +32,11 @@ export const toggleIsFetching = (isFetching: boolean) => (
 export const toggleFollowingProgress = (inProgress: boolean, userId: number) => (
     {type: 'TOGGLE-IS-FOLLOWING-IN-PROGRESS', inProgress, userId} as const);
 
-export const getUsers = (currentPage: number, pageSize: number) => {
+export const requestUsers = (page: number, pageSize: number) => {
     return (dispatch: Dispatch<UsersActionCreatorTypes>) => {
         dispatch(toggleIsFetching(true));
-        usersAPI.getUsers(currentPage, pageSize).then(data => {
+        dispatch(setCurrentPage(page));
+        usersAPI.getUsers(page, pageSize).then(data => {
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items))
             dispatch(setUsersTotalCount(data.totalCount));
