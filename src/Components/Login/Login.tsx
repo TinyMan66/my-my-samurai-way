9ad07a82e1702ff7a9568cd45def59725f8c098b
Diana Lipanova <altere2804@gmail.com>
2022-11-27 19:58:34 +0100

    Created the createField function (refactored Login.tsx)

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Field, InjectedFormProps, reduxForm} from "redux-form";
-import {Input} from "../common/FormsControls/FormsControls";
+import {InjectedFormProps, reduxForm} from "redux-form";
+import {createField, Input} from "../common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {loginTC} from "../../redux/auth-reducer";
@@ -12,19 +12,14 @@ const mapStateToProps = (state: AppStateType): mapStatePropsType => ({
     isAuth: state.auth.isAuth
 })
 
-const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = ({handleSubmit, error}) => {
     return (
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <Field placeholder={"Email"} name={"email"} validate={[required]} component={Input}/>
-            </div>
-            <div>
-                <Field placeholder={"Password"} name={"password"} type={"password"} validate={[required]} component={Input}/>
-            </div>
-            <div>
-                <Field component={Input} name={"rememberMe"} type={"checkbox"}/> Remember me
-            </div>
-            {props.error && <div className={style.formSummaryError}>{props.error}</div>}
+        <form onSubmit={handleSubmit}>
+            {createField("Email", "email", [required], Input)}
+            {createField("Password", "password", [required], Input, {type: "password"})}
+            {createField(undefined, "rememberMe", [], Input, {type: "checkbox"}, "remember me")}
+
+            {error && <div className={style.formSummaryError}>{error}</div>}
 
             <div>
                 <button>Login</button>
@@ -58,12 +53,8 @@ type FormDataType = {
     password: string | null
     rememberMe: boolean
 }
-type mapStatePropsType = {
-    isAuth: boolean
-}
-
+type mapStatePropsType = { isAuth: boolean }
 type mapDispatchPropsType = {
     loginTC: (email: string | null, password: string | null, rememberMe: boolean) => void
 }
-
 type LoginPropsType = mapStatePropsType & mapDispatchPropsType
--- src/Components/common/FormsControls/FormsControls.tsx
+++ src/Components/common/FormsControls/FormsControls.tsx
@@ -1,31 +1,45 @@
 import React, {FC} from "react";
 import styles from "./FormsControls.module.css"
-import {WrappedFieldProps} from "redux-form";
+import {Field, WrappedFieldProps} from "redux-form";
+import {FieldValidatorType} from "../../../utils/validators/validators";
 
-const FormControl: FC<WrappedFieldProps> = ({meta, children}) => {
-    const hasError = meta.touched && meta.error
+const FormControl: FC<WrappedFieldProps> = ({meta: {touched, error}, children}) => {
+    const hasError = touched && error
 
     return (
         <div className={`${styles.formControl} ${hasError ? styles.error : ""}`}>
             <div>
                 {children}
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <span>{error}</span>}
         </div>
     )
 }
-export const Textarea: FC<
-    WrappedFieldProps> = (props) => {
+export const Textarea: FC<WrappedFieldProps> = (props) => {
     const {input, meta, children, ...restProps} = props
     return (
         <FormControl {...props}><textarea{...input}{...restProps}/></FormControl>
     )
 }
 
-export const Input: FC<
-    WrappedFieldProps> = (props) => {
+export const Input: FC<WrappedFieldProps> = (props) => {
     const {input, meta, ...restProps} = props
     return (
         <FormControl {...props}><input{...input}{...restProps}/></FormControl>
     )
 }
+
+export const createField = (placeholder: string | undefined,
+                            name: string,
+                            validators: FieldValidatorType[],
+                            component: React.FC<WrappedFieldProps>,
+                            props = {}, text: string = '') => (
+    <div>
+        <Field placeholder={placeholder}
+               name={name}
+               validate={validators}
+               component={component}
+               {...props}
+        /> {text}
+    </div>
+)
