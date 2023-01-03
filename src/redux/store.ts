cc09e9a757926cdab6b4d8814a256ff25a553753
Diana Lipanova <altere2804@gmail.com>
2023-01-03 14:48:26 +0100

    feat: add redux dev tool extension

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from "redux";
 import profileReducer, {ProfileActionTypes} from "./profile-reducer";
 import dialogsReducer, {DialogsActionTypes} from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer, {UsersActionTypes} from "./users-reducer";
 import authReducer, {AuthActionTypes} from "./auth-reducer";
-import thunkMiddleware, { ThunkAction } from "redux-thunk";
+import thunkMiddleware, {ThunkAction} from "redux-thunk";
 import {reducer as formReducer} from "redux-form"
 import {AppActionTypes, appReducer} from "./app-reducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -18,10 +19,16 @@ const rootReducer = combineReducers({
     app: appReducer
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const composeEnhancers = composeWithDevTools || compose;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export type AppStateType = ReturnType<typeof rootReducer>;
-export type AppActionsType = AppActionTypes | DialogsActionTypes | ProfileActionTypes | UsersActionTypes | AuthActionTypes
+export type AppActionsType =
+    AppActionTypes
+    | DialogsActionTypes
+    | ProfileActionTypes
+    | UsersActionTypes
+    | AuthActionTypes
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AppActionsType>
 
 // @ts-ignore
